f2d967d1caa75f242e6376992288d4c88a8f73c7
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2026-01-25 22:02:02 +0100

    - Redirect to Vercel deployment

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import HomeView from "./app/view/HomeView";
-
-export default function App() {
-  return <HomeView />;
-}
--- src/app/view/utils/youtube.ts
+++
@@ -1,9 +0,0 @@
-export function toYouTubeEmbedUrl(videoUrl: string, videoId: string) {
-  // safest: always use id for embed
-  return `https://www.youtube.com/embed/${encodeURIComponent(
-    videoId
-  )}?autoplay=1&rel=0&modestbranding=1`;
-
-  console.log(videoUrl);
-  
-}
